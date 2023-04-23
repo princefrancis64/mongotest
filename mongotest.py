aefbfabcd15910b31363adab95eb7ec34946573d
Prince Francis <prince.francis64@gmail.com>
2023-04-23 13:45:20 +0530

    this is my commit

---
+++ mongotest.py
@@ -0,0 +1,27 @@
+import pymongo
+
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://princefrancis64:Oejb2e2l74Gz4NAK@cluster0.o5qm0dq.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+print(client)
+
+d = {
+   "name":"prince",
+   "email":"prince.francis64@gmail.com",
+   "surname":"francis"
+}
+
+db1=client['mongotest']
+coll=db1['test']
+coll.insert_one(d)
